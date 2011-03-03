78d931a04fff54720f86af642b3645ab72a70ca9
kayiwadaniel <kayiwadaniel@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2011-03-03 11:26:58 +0000

    Adding more commands to the command history chain.

---
+++ branches/1.0.1/FormDesigner/src/org/purc/purcforms/client/cmd/DeleteFieldCmd.java
@@ -0,0 +1,62 @@
+package org.purc.purcforms.client.cmd;
+
+import org.purc.purcforms.client.view.FormsTreeView;
+
+import com.google.gwt.user.client.ui.TreeItem;
+
+/**
+ * 
+ * @author danielkayiwa
+ *
+ */
+public class DeleteFieldCmd implements ICommand {
+
+	private FormsTreeView view;
+	private TreeItem item;
+	private TreeItem parentItem;
+	private int index;
+
+
+	public DeleteFieldCmd(TreeItem item, TreeItem parentItem, int index, FormsTreeView view){
+		this.item = item;
+		this.parentItem = parentItem;
+		this.view = view;
+		this.index = index;
+	}
+
+	public String getName(){
+		return "Delete Field";
+	}
+
+	public void undo(){
+		TreeItem inserAfterItem = null;
+		if(parentItem != null){
+			inserAfterItem = parentItem.getChild(index);
+			if(inserAfterItem != null)
+				parentItem.insertItem(item, inserAfterItem);
+			else
+				parentItem.addItem(item);	
+		}
+		else
+			view.addRootItem(item);
+		
+		view.addFormDefItem(item.getUserObject(), (inserAfterItem != null ? inserAfterItem.getUserObject() : null), parentItem);
+		view.selectItem(item);
+		
+		//if(index == 0 && parentItem != null)
+		//	view.moveItemUp();
+		
+		//add xforms nodes
+		//add to validation rules
+		//add to skip rules
+		//add to dynamic lists
+	}
+
+	public void redo(){
+		view.deleteItem(item, parentItem);
+	}
+	
+	public boolean isWidgetCommand(){
+		return false;
+	}
+}
