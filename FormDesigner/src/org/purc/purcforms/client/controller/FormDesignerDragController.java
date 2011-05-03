7300b695c72a5152494d03116379a5df9b12d90b
kayiwadaniel <kayiwadaniel@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2011-05-03 22:34:27 +0000

    Fixing bug where resizing group box widgets did not have command history.

--- FormDesigner/src/org/purc/purcforms/client/controller/FormDesignerDragController.java
+++ FormDesigner/src/org/purc/purcforms/client/controller/FormDesignerDragController.java
@@ -207,7 +207,9 @@ public class FormDesignerDragController extends AbstractDragController{
 						cursor = DOM.getStyleAttribute(((DesignGroupWidget)((DesignWidgetWrapper)context.draggable).getWrappedWidget()).getHeaderLabel().getWrappedWidget().getElement(), "cursor");
 				}
 
-				if("move".equals(cursor) || "".equals(cursor)){
+				if(("move".equals(cursor) || "".equals(cursor)) && 
+				   (context.selectedWidgets.size() > 0 && !(((DesignWidgetWrapper)context.selectedWidgets.get(0)).getWrappedWidget() instanceof DesignGroupWidget))){
+					
 					for (Widget widget : context.selectedWidgets) {
 						SavedWidgetInfo info = savedWidgetInfoMap.get(widget);
 						int x = info.initialDraggableParentLocation.getLeft() - ((DesignWidgetWrapper)widget).getLeftInt();
