6a1aee0315f1699e7b23264e0615b474df9e5f37
kayiwadaniel <kayiwadaniel@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2011-10-12 15:06:14 +0000

    Fixing IndexOutOfBoundsException when loading some multipage xforms.

--- SharedLib/src/org/purc/purcforms/client/xforms/XformParser.java
+++ SharedLib/src/org/purc/purcforms/client/xforms/XformParser.java
@@ -431,7 +431,7 @@ public class XformParser {
 
 				setQuestionDataNode(questionDef,formDef,parentQtn);
 			}
-			else{
+			else if(!(questionDef.getParent() instanceof PageDef)){
 				PageDef pageDef = formDef.setPageName(nodeContext.getLabel());
 				pageDef.setLabelNode(nodeContext.getLabelNode());
 				pageDef.setPageNo(getNextPageNo());
