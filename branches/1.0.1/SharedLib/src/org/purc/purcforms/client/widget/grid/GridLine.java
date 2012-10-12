ae0185da836e28b308f619ac5692eb7f48bac8e3
kayiwadaniel <kayiwadaniel@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2012-10-12 02:58:30 +0000

    Adding table widget that has grid lines

---
+++ branches/1.0.1/SharedLib/src/org/purc/purcforms/client/widget/grid/GridLine.java
@@ -0,0 +1,7 @@
+package org.purc.purcforms.client.widget.grid;
+
+import com.google.gwt.user.client.ui.HTML;
+
+public class GridLine extends HTML {
+
+}
---
+++ branches/1.0.1/SharedLib/src/org/purc/purcforms/client/widget/grid/HorizontalGridLine.java
@@ -0,0 +1,10 @@
+package org.purc.purcforms.client.widget.grid;
+
+public class HorizontalGridLine extends GridLine {
+    
+    public HorizontalGridLine(int width) {
+	setStylePrimaryName("purcforms-horizontal-grid-line");
+	setWidth(width + "px");
+    }
+
+}
