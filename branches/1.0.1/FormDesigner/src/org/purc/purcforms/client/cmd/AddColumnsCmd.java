de02c8c96c92d4a7b14f370586c602a7c71267b6
kayiwadaniel@gmail.com <kayiwadaniel@gmail.com@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2013-03-09 18:20:03 +0000

    Adding undo redo support for adding table rows and columns

--- branches/1.0.1/FormDesigner/src/org/purc/purcforms/client/cmd/AddColumnsCmd.java
+++ branches/1.0.1/FormDesigner/src/org/purc/purcforms/client/cmd/AddColumnsCmd.java
@@ -1,11 +1,62 @@
 package org.purc.purcforms.client.cmd;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.purc.purcforms.client.widget.DesignWidgetWrapper;
+import org.purc.purcforms.client.widget.grid.GridDesignGroupWidget;
+
 /**
  * Command for adding table columns.
  * 
  * @author danielkayiwa
  *
  */
-public class AddColumnsCmd {
+public class AddColumnsCmd implements ICommand {
 
+	private int xpos;
+	private boolean right;
+	private int totalDisplacement;
+	private GridDesignGroupWidget table;
+	private List<DesignWidgetWrapper> lines = new ArrayList<DesignWidgetWrapper>();
+	
+	public AddColumnsCmd(int xpos, boolean right, int totalDisplacement, GridDesignGroupWidget table) {
+		this.xpos = xpos;
+		this.right = right;
+		this.totalDisplacement = totalDisplacement;
+		this.table = table;
+	}
+	
+	public String getName(){
+		return "Add Columns";
+	}
+
+	public void undo(){
+		//remove all the newly added lines.
+		for(DesignWidgetWrapper line : lines) {
+			line.storePosition();
+			table.remove(line);
+		}
+		
+		table.moveVerticalLinesAndText(xpos, -totalDisplacement);
+		table.resizeHorizontalLinesAndTable(xpos, -totalDisplacement);
+	}
+
+	public void redo(){
+		table.moveVerticalLinesAndText(xpos, totalDisplacement);
+		table.resizeHorizontalLinesAndTable(xpos, totalDisplacement);
+		
+		//add the previously added lines.
+		for(DesignWidgetWrapper line : lines) {
+			table.add(line);
+		}
+	}
+	
+	public boolean isWidgetCommand(){
+		return true;
+	}
+	
+	public void addline(DesignWidgetWrapper line) {
+		lines.add(line);
+	}
 }
