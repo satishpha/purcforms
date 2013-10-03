5e69696a8008bf8391fd49479d998ac05981ea50
kayiwadaniel@gmail.com <kayiwadaniel@gmail.com@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2013-10-03 20:29:05 +0000

    Fixing compiler errors in query builder

--- branches/1.0.1/QueryBuilder/src/org/purc/purcforms/client/querybuilder/widget/FieldWidget.java
+++ branches/1.0.1/QueryBuilder/src/org/purc/purcforms/client/querybuilder/widget/FieldWidget.java
@@ -140,8 +140,8 @@ public class FieldWidget extends Composite{
 	private boolean selectFirstQuestion(Vector questions){
 		for(int i=0; i<questions.size(); i++){
 			QuestionDef questionDef = (QuestionDef)questions.elementAt(i);
-			if(questionDef.getDataType() == QuestionDef.QTN_TYPE_REPEAT)
-				selectFirstQuestion(questionDef.getRepeatQtnsDef().getQuestions());
+			if(questionDef.isGroupQtnsDef())
+				selectFirstQuestion(questionDef.getGroupQtnsDef().getQuestions());
 			else{
 				selectQuestion(questionDef);
 				return true;
