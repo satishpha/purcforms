0b2d85c61df59313ba88e37cbd47f6b464bd85b9
kayiwadaniel@gmail.com <kayiwadaniel@gmail.com@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2014-07-17 23:11:53 +0000

    Removing unused import

--- SharedLib/src/org/purc/purcforms/client/widget/RuntimeWidgetWrapper.java
+++ SharedLib/src/org/purc/purcforms/client/widget/RuntimeWidgetWrapper.java
@@ -37,7 +37,6 @@ import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
